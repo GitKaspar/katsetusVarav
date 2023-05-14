77d979e35c48a9f437ebcee99739ae35a9bcf896
GitKaspar <127048948+GitKaspar@users.noreply.github.com>
2023-05-15 01:16:51 +0300

    Update Mäng.java

--- src/Mäng.java
+++ src/Mäng.java
@@ -4,30 +4,44 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
 
+/**
+ * Peaklass. Kannab mängu isendimuutujaid, mida kasutavad kujundused ja hilisemad sündmused.
+ */
+
 public class Mäng {
 
     JFrame mänguAken;
-    Container konteiner;
+    Container konteiner; // Sisaldab paneele.
     JPanel pealkirjaPaneel, algusNupuPaneel, peamineTekstipaneel, valikunupuPaneel, mängijaPaneel, pildiPaneel;
     JLabel pealkirjaSilt, hpSilt, hpSildiNumber, relvaSilt, relvaSildiNimi, pildiSilt;
     Font pealkirjaFont = new Font("Comic Sans MS", Font.PLAIN, 90);
     Font tavalineFont = new Font("Comic Sans MS", Font.PLAIN, 28);
-    JButton algusNupp, valik1, valik2, valik3, valik4;
+    JButton algusNupp, valik1, valik2, valik3, valik4; // Mänguakna nupud
     JTextArea peamineTekstiRuum;
     int mängijaHP, koletiseHP, hõbesõrmus, kõrgus, laius;
     double xSuhe, ySuhe; // Nende muutujate järgi seatakse kasutajaliidese elementide suurus
-    String relv, asukoht;
+    String relv, asukoht; // Asukoha muutuja on sündmuste puhul hiljem tähtis.
 
+    // Sündmuste "kuulajad" üks tutvustavale aknale, teine pmänguaknale.
     PeaEkraaniKäsitleja peKäsitleja = new PeaEkraaniKäsitleja();
     ValikuKäsitleja valikuKäsitleja = new ValikuKäsitleja();
 
-
+    /**
+     * Peameetod loob vaid Mäng isendi. Tolle konstukror paneb kõik liikuma.
+     */
     public static void main(String[] args) {
 
         new Mäng();
 
     }
 
+    /**
+     * Siin luuakse mänguaken. Aken on täidab ekraani, aga omab raami.
+     * Et säilitada kujunduse terviklikkus jätsime esialgu akna suuruse fikseerituks.
+     * Küll aga kohaneb kasutajaliidese kujundus akna suurusega (vastavalt resolutsioonile).
+     * Kasutame kujunduses algselt õpetuses ette antud suurusi ja kohandame neid vastavalt...
+     * ... käesoleva masina akna resolutsioonile.
+     */
     public Mäng() {
 
         mänguAken = new JFrame();
@@ -39,24 +53,24 @@ public class Mäng {
         mänguAken.setLayout(null); // Välistab vaikeasetuse kasutamise.
         laius = mänguAken.getWidth();
         kõrgus = mänguAken.getHeight();
-        xSuhe = (double)laius / 800;
+        xSuhe = (double) laius / 800;
         ySuhe = (double) kõrgus / 600;
         konteiner = mänguAken.getContentPane();
 
         pealkirjaPaneel = new JPanel();
-        pealkirjaPaneel.setBounds((int)(100*xSuhe), (int)(100*ySuhe), (int)(600*xSuhe), (int)(150*ySuhe)); // x ja y märgivad paneeli alguspunkti
+        pealkirjaPaneel.setBounds((int) (100 * xSuhe), (int) (100 * ySuhe), (int) (600 * xSuhe), (int) (150 * ySuhe)); // x ja y märgivad paneeli alguspunkti
         pealkirjaPaneel.setBackground(Color.black);
 
         ImageIcon pilt = new ImageIcon("pealkiri.png");
         Image pildiks = pilt.getImage();
-        Image suurusMuudetud = pildiks.getScaledInstance((int)(600*xSuhe), (int)(150*ySuhe), Image.SCALE_SMOOTH);
+        Image suurusMuudetud = pildiks.getScaledInstance((int) (600 * xSuhe), (int) (150 * ySuhe), Image.SCALE_SMOOTH);
         pealkirjaSilt = new JLabel(new ImageIcon(suurusMuudetud));
 
-        pealkirjaSilt.setForeground(Color.white); // Probleem, kui muudan paneeli valgeks ja teksti mustaks - ei kuva
+        pealkirjaSilt.setForeground(Color.white);
         pealkirjaSilt.setFont(pealkirjaFont);
 
         algusNupuPaneel = new JPanel();
-        algusNupuPaneel.setBounds((int)(300*xSuhe), (int)(500*ySuhe), (int)(200*xSuhe), (int)(100*ySuhe)); // 480, 280, 320, 120
+        algusNupuPaneel.setBounds((int) (300 * xSuhe), (int) (500 * ySuhe), (int) (200 * xSuhe), (int) (100 * ySuhe));
         algusNupuPaneel.setBackground(Color.black);
 
         algusNupp = new JButton("ALUSTA");
@@ -76,26 +90,33 @@ public class Mäng {
 
     }
 
+    /**
+     * Kui luuakse mänguaken, muudetakse peamenüü elemendid nähtamatuks.
+     * Mänguaken koosneb tekstialast, mängija staatuse ribast, valikunuppidest ja pildialast.
+     */
     public void looMänguEkraan() {
 
         pealkirjaPaneel.setVisible(false);
         algusNupuPaneel.setVisible(false);
 
+        // See ja järgmine lõik teksivälja kohta.
         peamineTekstipaneel = new JPanel();
-        peamineTekstipaneel.setBounds((int)(60*xSuhe), (int)(200*ySuhe), (int)(330*xSuhe), (int)(100*ySuhe));
+        peamineTekstipaneel.setBounds((int)(60*xSuhe), (int)(200*ySuhe), (int)(370*xSuhe), (int)(200*ySuhe));
         peamineTekstipaneel.setBackground(Color.black);
         konteiner.add(peamineTekstipaneel);
 
         peamineTekstiRuum = new JTextArea();
-        peamineTekstiRuum.setBounds((int)(60*xSuhe), (int)(200*ySuhe), (int)(330*xSuhe), (int)(100*ySuhe));
+        peamineTekstiRuum.setBounds((int)(60*xSuhe), (int)(200*ySuhe), (int)(370*xSuhe), (int)(200*ySuhe));
         peamineTekstiRuum.setBackground(Color.black);
         peamineTekstiRuum.setForeground(Color.white);
         peamineTekstiRuum.setFont(tavalineFont);
         peamineTekstiRuum.setLineWrap(true);
         peamineTekstipaneel.add(peamineTekstiRuum);
 
+        // Siin luuakse valikupaneel ja selle nupud.
+        // Nupud seotakse kõik sündmuste kuulajaga, et need hiljem reageerida saaksid.
         valikunupuPaneel = new JPanel();
-        valikunupuPaneel.setBounds((int)(60*xSuhe), (int)(400*ySuhe), (int)(300*xSuhe), (int)(150*ySuhe));
+        valikunupuPaneel.setBounds((int) (60 * xSuhe), (int) (400 * ySuhe), (int) (300 * xSuhe), (int) (150 * ySuhe));
         valikunupuPaneel.setBackground(Color.black);
         valikunupuPaneel.setLayout(new GridLayout(4, 1));
         konteiner.add(valikunupuPaneel);
@@ -136,8 +157,9 @@ public class Mäng {
         valik4.addActionListener(valikuKäsitleja);
         valik4.setActionCommand("v4");
 
+        // Järgneb mängijapaneel, kus kuvatakse elud ja relv.
         mängijaPaneel = new JPanel();
-        mängijaPaneel.setBounds((int)(150*xSuhe), (int)(15*ySuhe), (int)(600*xSuhe), (int)(50*ySuhe));
+        mängijaPaneel.setBounds((int) (150 * xSuhe), (int) (15 * ySuhe), (int) (600 * xSuhe), (int) (50 * ySuhe));
         mängijaPaneel.setBackground(Color.black);
         mängijaPaneel.setLayout(new GridLayout(1, 4));
         konteiner.add(mängijaPaneel);
@@ -161,25 +183,26 @@ public class Mäng {
         relvaSildiNimi.setForeground(Color.white);
         mängijaPaneel.add(relvaSildiNimi);
 
+        // Pildivälja algväärtustamine
         ImageIcon lennart = new ImageIcon("lennart.png");
         Image kohandamiseks = lennart.getImage();
-        Image kohandatudPilt = kohandamiseks.getScaledInstance((int)(350*xSuhe), (int)(350*ySuhe), Image.SCALE_SMOOTH);
+        Image kohandatudPilt = kohandamiseks.getScaledInstance((int) (350 * xSuhe), (int) (350 * ySuhe), Image.SCALE_SMOOTH);
         pildiSilt = new JLabel(new ImageIcon(kohandatudPilt));
 
         pildiPaneel = new JPanel();
-        pildiPaneel.setBounds((int)(400*xSuhe), (int)(90*ySuhe), (int)(350*xSuhe), (int)(350*ySuhe)); // 480, 280, 320, 120
+        pildiPaneel.setBounds((int) (400 * xSuhe), (int) (90 * ySuhe), (int) (350 * xSuhe), (int) (350 * ySuhe)); // 480, 280, 320, 120
         pildiPaneel.setBackground(Color.black);
         pildiPaneel.add(pildiSilt);
         konteiner.add(pildiPaneel);
 
-
-
-
         mängijaEttevalmistus();
-
 
     }
 
+    /**
+     * Meetod algväärstustab erinevad mängija tunnused ning alustab esimese meetodiga.
+     * Edasi kulgeb mäng vastavalt sellele, milliseid nuppe mängija erinevates meetodites vajutab.
+     */
     public void mängijaEttevalmistus() {
         mängijaHP = 15;
         koletiseHP = 20;
@@ -223,6 +246,7 @@ public class Mäng {
         valik4.setVisible(false);
     }
 
+    // Keskne punkt mängus, kust saab kõikjale edasi liikuda.
     public void risttee() {
         asukoht = "risttee";
         peamineTekstiRuum.setText("Oled ristteel.\nLinn jääb lõunasse.");
@@ -238,6 +262,8 @@ public class Mäng {
         valik4.setText("Mine lääne suunas.");
     }
 
+    // Jõe ääres saab puhata ja elusid taastada.
+
     public void põhi() {
         asukoht = "põhi";
         peamineTekstiRuum.setText("Jõuad jõe äärde. Jood vett ja puhkad jõekaldal.\n(Su elujõud taastub kahe võrra.)");
@@ -249,6 +275,7 @@ public class Mäng {
         valik4.setVisible(false);
     }
 
+    // Siit saab mõõga, mis aitab koletist alistada.
     public void ida() {
         asukoht = "ida";
         peamineTekstiRuum.setText("Sattusid metsa ja leidsid mõõga.\n(Sain enda valdusesse uue relva: mõõk.)");
@@ -261,6 +288,7 @@ public class Mäng {
 
     }
 
+    // Siin on koletis, kellega peab võitlema.
     public void lääs() {
         asukoht = "lääs";
         peamineTekstiRuum.setText("Juhtud kokku mäekolliga.");
@@ -303,9 +331,8 @@ public class Mäng {
 
     public void koletisRündab() {
         asukoht = "koletisRündab";
-        int koletiseKahju = 0;
 
-        koletiseKahju = new Random().nextInt(6);
+        int koletiseKahju = new Random().nextInt(6);
 
         peamineTekstiRuum.setText("Koletis ründas sind ja tekitas " + koletiseKahju + " punkti kahju.");
 
@@ -317,9 +344,11 @@ public class Mäng {
         valik3.setVisible(false);
         valik4.setVisible(false);
 
-
     }
 
+    /**
+     * Võidutingimus käivitub, kui alistatakse koletis. Siit edasi peab võitmiseks tagasi värava juurde minema.
+     */
     public void võit() {
         asukoht = "võit";
         peamineTekstiRuum.setText("Alistasid koletise.\nLeiad koletise korjuselt sõrmuse.\n\n(Omandasid hõbedase sõrmuse.)");
@@ -330,6 +359,9 @@ public class Mäng {
         valik4.setVisible(false);
     }
 
+    /**
+     * Kaotad siis, kui koletis või valvur su ära tapavad.
+     */
     public void kaotus() {
         asukoht = "kaotus";
         peamineTekstiRuum.setText("Oled surnud.\nMÄNG LÄBI");
@@ -338,9 +370,9 @@ public class Mäng {
         valik3.setVisible(false);
         valik4.setVisible(false);
 
-
     }
 
+    // Eduka lõpu meetod.
     public void lõpp() {
         asukoht = "lõpp";
         peamineTekstiRuum.setText("Valvur: Sul on sõrmus! Alistasid sa mäekolli?\nLinnapea tahab sind kindlasti selle eest permeerida. Järgne mulle.\n(Valvur avab värava ja juhatab su linna.)\nMÄNG LÄBI");
@@ -351,6 +383,9 @@ public class Mäng {
 
     }
 
+    /**
+     * Loob mänguakna, kui vajutatakse nuppu "Alusta".
+     */
     public class PeaEkraaniKäsitleja implements ActionListener {
 
         @Override
@@ -359,6 +394,10 @@ public class Mäng {
         }
     }
 
+    /**
+     * Klass, mis jälgib tegevusi nuppudega. Nuppudele vastavad tegevused on seotud mängija asukohaga.
+     * Vastavalt asukohale on mängijal üks või mitu valikut.
+     */
     public class ValikuKäsitleja implements ActionListener {
 
         public void actionPerformed(ActionEvent e) {
@@ -366,118 +405,88 @@ public class Mäng {
             String sinuValik = e.getActionCommand();
 
             switch (asukoht) {
-                case "linnaVärav":
+                case "linnaVärav" -> {
                     switch (sinuValik) {
-                        case "v1":
+                        case "v1" -> {
                             if (hõbesõrmus == 1) {
                                 lõpp();
                             } else {
                                 räägiValvuriga();
                             }
-                            break;
-                        case "v2":
+                        }
+                        case "v2" -> {
                             if (mängijaHP < 1) {
                                 kaotus();
                             } else {
                                 ründaValvurit();
-                            } break;
-                        case "v3":
-                            risttee();
-                            break;
+                            }
+                        }
+                        case "v3" -> risttee();
                     }
-                    break;
-                case "räägiValvuriga":
-                    switch (sinuValik) {
-                        case "v1":
-                            linnaVärav();
-                            break;
+                }
+                case "räägiValvuriga" -> {
+                    if (sinuValik.equals("v1")) {
+                        linnaVärav();
                     }
-                    break;
-                case "ründaValvurit":
-                    switch (sinuValik) {
-                        case "v1":
-                            linnaVärav();
-                            break;
+                }
+                case "ründaValvurit" -> {
+                    if (sinuValik.equals("v1")) {
+                        linnaVärav();
                     }
-                    break;
-                case "risttee":
+                }
+                case "risttee" -> {
                     switch (sinuValik) {
-                        case "v1":
-                            põhi();
-                            break;
-                        case "v2":
-                            ida();
-                            break;
-                        case "v3":
-                            linnaVärav();
-                            break;
-                        case "v4":
-                            lääs();
-                            break;
+                        case "v1" -> põhi();
+                        case "v2" -> ida();
+                        case "v3" -> linnaVärav();
+                        case "v4" -> lääs();
                     }
-                    break;
-                case "põhi":
-                    switch (sinuValik) {
-                        case "v1":
-                            risttee();
-                            break;
+                }
+                case "põhi" -> {
+                    if (sinuValik.equals("v1")) {
+                        risttee();
                     }
-                    break;
-                case "ida":
-                    switch (sinuValik) {
-                        case "v1":
-                            risttee();
-                            break;
+                }
+                case "ida" -> {
+                    if (sinuValik.equals("v1")) {
+                        risttee();
                     }
-                    break;
-                case "lääs":
+                }
+                case "lääs" -> {
                     switch (sinuValik) {
-                        case "v1":
+                        case "v1" -> võitle();
+                        case "v2" -> risttee();
+                    }
+                }
+                case "võitlus" -> {
+                    switch (sinuValik) {
+                        case "v1" -> mängijaRündab();
+                        case "v2" -> risttee();
+                    }
+                }
+                case "mängijaRündab" -> {
+                    if (sinuValik.equals("v1")) {
+                        if (koletiseHP < 1) {
+                            võit();
+                        } else {
+                            koletisRündab();
+                        }
+                    }
+                }
+                case "koletisRündab" -> {
+                    if (sinuValik.equals("v1")) {
+                        if (mängijaHP < 1) {
+                            kaotus();
+                        } else {
                             võitle();
-                            break;
-                        case "v2":
-                            risttee();
-                            break;
+                        }
                     }
-                    break;
-                case "võitlus":
-                    switch (sinuValik) {
-                        case "v1":
-                            mängijaRündab();
-                            break;
-                        case "v2":
-                            risttee();
-                            break;
+                }
+                case "võit" -> {
+                    if (sinuValik.equals("v1")) {
+                        risttee();
                     }
-                    break;
-                case "mängijaRündab":
-                    switch (sinuValik) {
-                        case "v1":
-                            if (koletiseHP < 1) {
-                                võit();
-                            } else {
-                                koletisRündab();
-                            }
-                            break;
-                    }
-                    break;
-                case "koletisRündab":
-                    switch (sinuValik) {
-                        case "v1":
-                            if (mängijaHP < 1) {
-                                kaotus();
-                            } else {
-                                võitle();
-                            }
-                            break;
-                    }
-                    break;
-                case "võit":
-                    switch (sinuValik) {
-                        case "v1":
-                            risttee();
-                    }
-                    break;
+                }
             }
         }
     }
